c9dbda9723a1db1be897bc6e91f906926d6f2981
Андрущенко Людмила <84805197+milaandrushchenko@users.noreply.github.com>
2023-07-05 12:06:15 +0300

    add some changes

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
 // Titles: https://omdbapi.com/?s=thor&page=1&apikey=fc1fef96
 // details: http://www.omdbapi.com/?i=tt3896198&apikey=fc1fef96
 
-const movieSearchBox = document.getElementById('movie-search-box');
-const searchList = document.getElementById('search-list');
-const resultGrid = document.getElementById('result-grid');
-
-
+const movieSearchBox = document.getElementById("movie-search-box");
+const searchList = document.getElementById("search-list");
+const resultGrid = document.getElementById("result-grid");
 
 // load movies from API
 async function loadMovies(search) {
-    const URL = `https://omdbapi.com/?s=${search}&page=1&apikey=fc1fef96`;
-    const response = await fetch(`${URL}`);
-    const data = await response.json();
-    // console.log(data);
-    if (data.Response == 'True') displayMovieList(data.Search);
+  const URL = `https://omdbapi.com/?s=${search}&page=1&apikey=fc1fef96`;
+  const response = await fetch(`${URL}`);
+  const data = await response.json();
+  // console.log(data);
+  if (data.Response == "True") displayMovieList(data.Search);
 }
 
-
 function findMovies() {
-    let search = movieSearchBox.value.trim();
-    if (search.length > 0) {
-        searchList.classList.remove('hide-search-list');
-        loadMovies(search);
-    } else {
-        searchList.classList.add('hide-search-list');
-    }
+  let search = movieSearchBox.value.trim();
+  if (search.length > 0) {
+    searchList.classList.remove("hide-search-list");
+    loadMovies(search);
+  } else {
+    searchList.classList.add("hide-search-list");
+  }
 }
 
 function displayMovieList(movies) {
-    searchList.innerHTML = '';
-    for (let movie of movies) {
-        let movieListItem = document.createElement('div');
-        movieListItem.dataset.id = movie.imdbID;
-        movieListItem.classList.add('search-list-item');
-        moviePoster = checkPoster(movie);
-        movieListItem.innerHTML = `
+  searchList.innerHTML = "";
+  for (let movie of movies) {
+    let movieListItem = document.createElement("div");
+    movieListItem.dataset.id = movie.imdbID;
+    movieListItem.classList.add("search-list-item");
+    moviePoster = checkPoster(movie);
+    movieListItem.innerHTML = `
                         <div class="search-item-thumbnail">
                             <img src="${moviePoster}">
                         </div>
@@ -43,27 +40,28 @@ function displayMovieList(movies) {
                             <p>${movie.Year}</p>
                         </div>
                         `;
-        searchList.appendChild(movieListItem);
-    }
-    loadMovieDetails();
+    searchList.appendChild(movieListItem);
+  }
+  loadMovieDetails();
 }
 
 function loadMovieDetails() {
-    const searchListMovies = searchList.querySelectorAll('.search-list-item');
-    searchListMovies.forEach(movie => {
-        movie.addEventListener('click', async () => {
-            searchList.classList.add('hide-search-list');
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fc1fef96
+  const searchListMovies = searchList.querySelectorAll(".search-list-item");
+  searchListMovies.forEach((movie) => {
+    movie.addEventListener("click", async () => {
+      searchList.classList.add("hide-search-list");
+      const result =
+        await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fc1fef96
             `);
-            const data = await result.json();
-            displayMovieDetails(data);
-        })
+      const data = await result.json();
+      displayMovieDetails(data);
     });
+  });
 }
 
 function displayMovieDetails(movie) {
-    moviePoster = checkPoster(movie);
-    resultGrid.innerHTML = `
+  moviePoster = checkPoster(movie);
+  resultGrid.innerHTML = `
                      <div class="movie-poster">
                         <img src="${moviePoster}" alt="movie poster">
                     </div>
@@ -87,15 +85,13 @@ function displayMovieDetails(movie) {
 }
 
 function checkPoster(movie) {
-    if (movie.Poster != 'N/A')
-        moviePoster = movie.Poster;
-    else
-        moviePoster = 'image_not_found.png';
-    return moviePoster;
+  if (movie.Poster != "N/A") moviePoster = movie.Poster;
+  else moviePoster = "image_not_found.png";
+  return moviePoster;
 }
 
-window.addEventListener('click', e => {
-    if (e.target.className != 'form-control') {
-        searchList.classList.add('hide-search-list');
-    }
-})
+window.addEventListener("click", (e) => {
+  if (e.target.className != "form-control") {
+    searchList.classList.add("hide-search-list");
+  }
+});
